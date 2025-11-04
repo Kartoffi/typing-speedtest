c75a3e9d533c4130abcfa298e8462cc71084c00a
Toffi <Stefanie.Reuter98@t-online.de>
2025-11-04 19:43:55 +0100

    Update styles for stats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,7 @@
 
     let wpm = $state(0);
     let cpm = $state(0);
+    let totalTime = 600;
     let time = $state(600);
 
     let correctTippedChars = $state(0);
@@ -59,10 +60,11 @@
     }
 
     let textArray = $state(createTextArray(text, 22));
+    let textArrayFlat = $state(textArray.flat());
 
 
     const recalcSpeed = () => {
-        let elapsedTime = 60 - time;
+        let elapsedTime = totalTime - time;
         if (elapsedTime > 0) {
             cpm = Math.floor(correctTippedChars / (elapsedTime / 60));
             wpm = Math.floor(cpm / 5);
@@ -113,6 +115,12 @@
             if (globalIndex + 1 >= text.length) {
                 testOver = true;
             }
+
+            // If last letter of row, move to next row
+            if (currentIndex >= rowLength) {
+                currentRow += 1;
+                currentIndex = 0;
+            }
             return;
         }
 
@@ -141,7 +149,7 @@
         return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
     }
 </script>
-<h1> Typing Speed-Test </h1>
+<h1> {testOver ? 'Test Over!' : 'Typing Speed-Test'} </h1>
 {#if !testOver}
     <div
         class="text"
@@ -168,6 +176,8 @@
                         </span>
                     {/each}
                 </div>
+            {:else}
+                <div class="row"></div>
             {/if}
             <div class="row">
                 {#each textArray[currentRow] as char, index}
@@ -193,40 +203,97 @@
                         </span>
                     {/each}
                 </div>
+            {:else}
+                <div class="row"></div>
             {/if}
         {/if}
     </div>
 {/if}
-<div> {secondsInMinutes(time)} seconds left</div>
+<div class="countdown">
+    <div class="countdown-timer">
+        {secondsInMinutes(time)}
+    </div>
+    <div class="countdown-progression-bar">
+        <div
+            class="countdown-progression-bar-fill"
+            style="width: {(time / totalTime) * 100}%"
+        ></div>
+    </div>
+</div>
 <input type="text" onkeydown={calculate} disabled={testOver} bind:this={inputRef}/>
+<div class="accuracy-container">
+    <div class="accuracy-area">
+        <div class="accuracy-percent">
+            {accuracy}
+        </div>
+        <svg class="accuracy" style="--progress: {accuracy}" viewBox="0 0 80 80" width="80" height="80">
+            <circle class="accuracy-bg"></circle>
+            <circle class="accuracy-fg"></circle>
+        </svg>
+    </div>
+    <div> Accuracy</div>
+</div>
+<div class="stats">
+    <div class="stats-container">
+        <div class="stats-value">
+            {wpm}
+        </div>
+        <div class="stats-text">
+            WPM
+        </div>
+    </div>
+    <div class="stats-container">
+        <div class="stats-value">
+            {cpm}
+        </div>
+        <div class="stats-text">
+            CPM
+        </div>
+    </div>
+    <div class="stats-container">
+        <div class="stats-value">
+            {falseTippedChars}
+        </div>
+        <div class="stats-text">
+            False Chars
+        </div>
+    </div>
+    <div class="stats-container">
+        <div class="stats-value">
+            {correctTippedChars + falseTippedChars}
+        </div>
+        <div class="stats-text">
+            Total Chars
+        </div>
+    </div>
+</div>
 {#if testOver}
-    <h2> Test Over! </h2>
-    <p>
+    <div class="overview">
         {#each textArray as row}
-            {#each row as char}
-                <span
-                    class="char"
-                    class:char--correct={char.correct === true}
-                    class:char--incorrect={char.correct === false}
-                >
-                    {char.char}
-                </span>
-            {/each}
-            <br/>
+            <div class="overview-row">
+                {#each row as char}
+                    <span
+                        class="overview-char"
+                        class:char--correct={char.correct === true}
+                        class:char--incorrect={char.correct === false}
+                    >
+                        {char.char}
+                    </span>
+                {/each}
+            </div>
         {/each}
-    </p>
+    </div>
 {/if}
-<div> {wpm} WPM </div>
-<div> {cpm} CPM </div>
-<div> Accuracy: {accuracy}</div>
-<div> False chars: {falseTippedChars}</div>
-<div> Total chars: {correctTippedChars + falseTippedChars}</div>
 
 <style lang="scss">
     input {
         opacity: 0;
         position: absolute;
         pointer-events: none;
+    }
+
+    h1 {
+        font-size: 2.5rem;
     }
 
     .char {
@@ -255,6 +322,7 @@
         align-items: center;
         justify-content: space-between;
         border-bottom: 1px solid lightgrey;
+        height: 30px;
     }
 
     .text {
@@ -270,5 +338,140 @@
         display: flex;
         flex-direction: column;
         gap: 16px;
+        margin: 30px;
+    }
+
+    .overview {
+        margin-top: 20px;
+        width: calc(100% - 80px);
+        border: 1px solid rgba(194, 194, 194, 0.308);
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+
+        &-row {
+            font-size: 1rem;
+            border-bottom: 1px solid lightgrey;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            height: 30px;
+        }
+
+        &-char {
+            display: inline-block;
+            font-weight: bold;
+
+            &--incorrect {
+                color: rgb(255, 49, 100);
+            }
+        }
+    }
+
+    .countdown {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 10px;
+        width: 100%;
+        margin: 20px 0;
+
+        &-timer {
+            font-size: 2rem;
+            font-weight: bold;
+            letter-spacing: 6px;
+        }
+
+        &-progression-bar {
+            width: 80%;
+            height: 10px;
+            background-color: lightgray;
+            border-radius: 5px;
+            box-shadow: inset 0 1px 2px rgba(0,0,0,.075);
+
+            &-fill {
+                height: 100%;
+                background-color: rgb(172, 49, 255);
+                border-radius: 5px;
+                transition: width 1s linear;
+            }
+        }
+    }
+
+    .accuracy {
+        --size: 80px;
+        --half-size: calc(var(--size) / 2);
+        --stroke-width: 8px;
+        --radius: calc((var(--size) - var(--stroke-width)) / 2);
+        --circumference: calc(var(--radius) * pi * 2);
+        --dash: calc((var(--progress) * var(--circumference)) / 100);
+
+        &-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 10px;
+            margin-top: 30px;
+        }
+
+        & circle {
+            cx: var(--half-size);
+            cy: var(--half-size);
+            r: var(--radius);
+            fill: transparent;
+            stroke-width: var(--stroke-width);
+            stroke-linecap: round;
+        }
+
+        &-bg {
+            stroke: #ddd;
+        }
+
+        &-fg {
+            stroke: rgb(172, 49, 255);
+            stroke-dasharray: var(--circumference);
+            stroke-dashoffset: calc(var(--circumference) - var(--dash));
+            transition: stroke-dashoffset 0.5s ease;
+            transform: rotate(-90deg);
+            transform-origin: var(--half-size) var(--half-size);
+        }
+
+        &-area {
+            position: relative;
+            width: 80px;
+            height: 80px;    
+        }
+
+        &-percent {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+    }
+
+    .stats {
+        margin-top: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        &-container {
+            width: 80px;
+            height: 80px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 5px;
+        }
+
+        &-value {
+            font-size: 2rem;
+            font-weight: bold;
+            text-align: center;
+        }
     }
 </style>
